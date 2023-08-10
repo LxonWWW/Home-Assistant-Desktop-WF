bf87f33d065dd09560935b6ae0e38c448b42204a
Lxon's Playground <service@jungleofgames.de>
2023-08-10 23:30:28 +0200

    Add files via upload

---
+++ Home Assistant Desktop/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace Home_Assistant_Desktop
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            mainWebView = new Microsoft.Web.WebView2.WinForms.WebView2();
+            notifyIcon1 = new NotifyIcon(components);
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            itemOpenInBrowser = new ToolStripMenuItem();
+            toolStripSeparator1 = new ToolStripSeparator();
+            itemAlignViewTopLeft = new ToolStripMenuItem();
+            itemAlignViewTopRight = new ToolStripMenuItem();
+            itemAlignViewBottomLeft = new ToolStripMenuItem();
+            itemAlignViewBottomRight = new ToolStripMenuItem();
+            toolStripSeparator4 = new ToolStripSeparator();
+            itemSetStartURL = new ToolStripMenuItem();
+            itemStayOnTop = new ToolStripMenuItem();
+            toolStripSeparator2 = new ToolStripSeparator();
+            itemRestartApplication = new ToolStripMenuItem();
+            itemResetApplication = new ToolStripMenuItem();
+            toolStripSeparator3 = new ToolStripSeparator();
+            itemSaveCurrentSettings = new ToolStripMenuItem();
+            toolStripSeparator5 = new ToolStripSeparator();
+            itemQuit = new ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)mainWebView).BeginInit();
+            contextMenuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // mainWebView
+            // 
+            mainWebView.AllowExternalDrop = true;
+            mainWebView.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            mainWebView.CreationProperties = null;
+            mainWebView.DefaultBackgroundColor = Color.White;
+            mainWebView.Location = new Point(-1, 0);
+            mainWebView.Name = "mainWebView";
+            mainWebView.Size = new Size(389, 812);
+            mainWebView.Source = new Uri("http://homeassistant.local:8123", UriKind.Absolute);
+            mainWebView.TabIndex = 0;
+            mainWebView.ZoomFactor = 1D;
+            // 
+            // notifyIcon1
+            // 
+            notifyIcon1.BalloonTipText = "Click here to Open";
+            notifyIcon1.BalloonTipTitle = "Home Assistant Desktop";
+            notifyIcon1.ContextMenuStrip = contextMenuStrip1;
+            notifyIcon1.Icon = (Icon)resources.GetObject("notifyIcon1.Icon");
+            notifyIcon1.Text = "Home Assistant Desktop";
+            notifyIcon1.Visible = true;
+            notifyIcon1.MouseClick += notifyIcon1_MouseClick;
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.BackColor = Color.FromArgb(17, 17, 17);
+            contextMenuStrip1.Font = new Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            contextMenuStrip1.Items.AddRange(new ToolStripItem[] { itemOpenInBrowser, toolStripSeparator1, itemAlignViewTopLeft, itemAlignViewTopRight, itemAlignViewBottomLeft, itemAlignViewBottomRight, toolStripSeparator4, itemSetStartURL, itemStayOnTop, toolStripSeparator2, itemRestartApplication, itemResetApplication, toolStripSeparator3, itemSaveCurrentSettings, toolStripSeparator5, itemQuit });
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.RenderMode = ToolStripRenderMode.System;
+            contextMenuStrip1.Size = new Size(205, 298);
+            // 
+            // itemOpenInBrowser
+            // 
+            itemOpenInBrowser.ForeColor = Color.WhiteSmoke;
+            itemOpenInBrowser.Name = "itemOpenInBrowser";
+            itemOpenInBrowser.Size = new Size(204, 22);
+            itemOpenInBrowser.Text = "Open in Browser";
+            itemOpenInBrowser.Click += itemOpenInBrowser_Click;
+            // 
+            // toolStripSeparator1
+            // 
+            toolStripSeparator1.Name = "toolStripSeparator1";
+            toolStripSeparator1.Size = new Size(201, 6);
+            // 
+            // itemAlignViewTopLeft
+            // 
+            itemAlignViewTopLeft.ForeColor = Color.WhiteSmoke;
+            itemAlignViewTopLeft.Name = "itemAlignViewTopLeft";
+            itemAlignViewTopLeft.Size = new Size(204, 22);
+            itemAlignViewTopLeft.Text = "Align View Top Left";
+            itemAlignViewTopLeft.Click += itemAlignViewTopLeft_Click;
+            // 
+            // itemAlignViewTopRight
+            // 
+            itemAlignViewTopRight.ForeColor = Color.WhiteSmoke;
+            itemAlignViewTopRight.Name = "itemAlignViewTopRight";
+            itemAlignViewTopRight.Size = new Size(204, 22);
+            itemAlignViewTopRight.Text = "Align View Top Right";
+            itemAlignViewTopRight.Click += itemAlignViewTopRight_Click;
+            // 
+            // itemAlignViewBottomLeft
+            // 
+            itemAlignViewBottomLeft.ForeColor = Color.WhiteSmoke;
+            itemAlignViewBottomLeft.Name = "itemAlignViewBottomLeft";
+            itemAlignViewBottomLeft.Size = new Size(204, 22);
+            itemAlignViewBottomLeft.Text = "Align View Bottom Left";
+            itemAlignViewBottomLeft.Click += itemAlignViewBottomLeft_Click;
+            // 
+            // itemAlignViewBottomRight
+            // 
+            itemAlignViewBottomRight.ForeColor = Color.WhiteSmoke;
+            itemAlignViewBottomRight.Name = "itemAlignViewBottomRight";
+            itemAlignViewBottomRight.Size = new Size(204, 22);
+            itemAlignViewBottomRight.Text = "Align View Bottom Right";
+            itemAlignViewBottomRight.Click += itemAlignViewBottomRight_Click;
+            // 
+            // toolStripSeparator4
+            // 
+            toolStripSeparator4.Name = "toolStripSeparator4";
+            toolStripSeparator4.Size = new Size(201, 6);
+            // 
+            // itemSetStartURL
+            // 
+            itemSetStartURL.ForeColor = Color.WhiteSmoke;
+            itemSetStartURL.Name = "itemSetStartURL";
+            itemSetStartURL.Size = new Size(204, 22);
+            itemSetStartURL.Text = "Set Start URL";
+            itemSetStartURL.Click += itemSetStartURL_Click;
+            // 
+            // itemStayOnTop
+            // 
+            itemStayOnTop.ForeColor = Color.WhiteSmoke;
+            itemStayOnTop.Name = "itemStayOnTop";
+            itemStayOnTop.Size = new Size(204, 22);
+            itemStayOnTop.Text = "Stay on Top";
+            itemStayOnTop.Click += itemStayOnTop_Click;
+            // 
+            // toolStripSeparator2
+            // 
+            toolStripSeparator2.Name = "toolStripSeparator2";
+            toolStripSeparator2.Size = new Size(201, 6);
+            // 
+            // itemRestartApplication
+            // 
+            itemRestartApplication.ForeColor = Color.WhiteSmoke;
+            itemRestartApplication.Name = "itemRestartApplication";
+            itemRestartApplication.Size = new Size(204, 22);
+            itemRestartApplication.Text = "Restart Application";
+            itemRestartApplication.Click += itemRestartApplication_Click;
+            // 
+            // itemResetApplication
+            // 
+            itemResetApplication.ForeColor = Color.WhiteSmoke;
+            itemResetApplication.Name = "itemResetApplication";
+            itemResetApplication.Size = new Size(204, 22);
+            itemResetApplication.Text = "⚠ Reset Application";
+            itemResetApplication.Click += itemResetApplication_Click;
+            // 
+            // toolStripSeparator3
+            // 
+            toolStripSeparator3.Name = "toolStripSeparator3";
+            toolStripSeparator3.Size = new Size(201, 6);
+            // 
+            // itemSaveCurrentSettings
+            // 
+            itemSaveCurrentSettings.ForeColor = Color.WhiteSmoke;
+            itemSaveCurrentSettings.Name = "itemSaveCurrentSettings";
+            itemSaveCurrentSettings.Size = new Size(204, 22);
+            itemSaveCurrentSettings.Text = "Save Current Settings";
+            itemSaveCurrentSettings.Click += itemSaveCurrentSettings_Click;
+            // 
+            // toolStripSeparator5
+            // 
+            toolStripSeparator5.Name = "toolStripSeparator5";
+            toolStripSeparator5.Size = new Size(201, 6);
+            // 
+            // itemQuit
+            // 
+            itemQuit.ForeColor = Color.WhiteSmoke;
+            itemQuit.Name = "itemQuit";
+            itemQuit.Size = new Size(204, 22);
+            itemQuit.Text = "Quit";
+            itemQuit.Click += itemQuit_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoSize = true;
+            BackColor = Color.FromArgb(17, 17, 17);
+            ClientSize = new Size(441, 811);
+            Controls.Add(mainWebView);
+            FormBorderStyle = FormBorderStyle.None;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "Form1";
+            ShowIcon = false;
+            ShowInTaskbar = false;
+            StartPosition = FormStartPosition.Manual;
+            Text = "Form1";
+            Deactivate += Form1_Deactivate;
+            Load += Form1_Load;
+            SizeChanged += Form1_SizeChanged;
+            ((System.ComponentModel.ISupportInitialize)mainWebView).EndInit();
+            contextMenuStrip1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Microsoft.Web.WebView2.WinForms.WebView2 mainWebView;
+        private NotifyIcon notifyIcon1;
+        private ContextMenuStrip contextMenuStrip1;
+        private ToolStripMenuItem itemOpenInBrowser;
+        private ToolStripSeparator toolStripSeparator1;
+        private ToolStripMenuItem itemStayOnTop;
+        private ToolStripMenuItem itemStartAtLogin;
+        private ToolStripSeparator toolStripSeparator2;
+        private ToolStripMenuItem itemRestartApplication;
+        private ToolStripMenuItem itemResetApplication;
+        private ToolStripSeparator toolStripSeparator3;
+        private ToolStripMenuItem itemQuit;
+        private ToolStripMenuItem itemAlignViewTopLeft;
+        private ToolStripMenuItem itemAlignViewTopRight;
+        private ToolStripMenuItem itemAlignViewBottomLeft;
+        private ToolStripMenuItem itemAlignViewBottomRight;
+        private ToolStripSeparator toolStripSeparator4;
+        private ToolStripMenuItem itemSaveCurrentSettings;
+        private ToolStripSeparator toolStripSeparator5;
+        private ToolStripMenuItem itemSetStartURL;
+        private Button button1;
+    }
+}
---
+++ Home Assistant Desktop/Form1.cs
@@ -0,0 +1,349 @@
+﻿using Home_Assistant_Desktop.Properties;
+using Microsoft.VisualBasic;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Home_Assistant_Desktop
+{
+    public partial class Form1 : Form
+    {
+        string currentViewPosition = "bottomRight";
+        Size currentViewSize = new Size(441, 811);
+        string currentStartURL = "http://homeassistant.local:8123";
+        Boolean currentStayOnTop = true;
+
+        // START Windows API for Window Resizing while having no borders
+
+        public const uint WM_NCPAINT = 0x85;
+        public const uint WM_NCCALCSIZE = 0x83;
+        public const uint WM_NCHITTEST = 0x84;
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int left, top, right, bottom;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct WINDOWPOS
+        {
+            public IntPtr hwnd;
+            public IntPtr hwndinsertafter;
+            public int x, y, cx, cy;
+            public int flags;
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct NCCALCSIZE_PARAMS
+        {
+            public RECT rgrc0, rgrc1, rgrc2;
+            public WINDOWPOS lppos;
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (DesignMode)
+                base.WndProc(ref m);
+
+            switch ((uint)m.Msg)
+            {
+                case WM_NCCALCSIZE: WmNCCalcSize(ref m); break;
+                default:
+                    base.WndProc(ref m);
+                    break;
+            }
+        }
+
+        private void WmNCCalcSize(ref Message m)
+        {
+            if (m.WParam != IntPtr.Zero)
+            {
+                var nccsp = (NCCALCSIZE_PARAMS)Marshal.PtrToStructure(m.LParam, typeof(NCCALCSIZE_PARAMS));
+
+                nccsp.rgrc0.top += 1;
+                nccsp.rgrc0.bottom -= 8;
+                nccsp.rgrc0.left += 8;
+                nccsp.rgrc0.right -= 8;
+
+                Marshal.StructureToPtr(nccsp, m.LParam, true);
+            }
+            else
+            {
+                var clnRect = (RECT)Marshal.PtrToStructure(m.LParam, typeof(RECT));
+
+                clnRect.top += 0;
+                clnRect.bottom -= 8;
+                clnRect.left += 8;
+                clnRect.right -= 8;
+
+                Marshal.StructureToPtr(clnRect, m.LParam, true);
+            }
+
+            m.Result = IntPtr.Zero;
+        }
+
+        // END Windows API for Window Resizing while having no borders
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadCurrentSettings();
+
+            this.FormBorderStyle = FormBorderStyle.Sizable;
+            this.Text = "";
+
+            showView(false);
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            setViewSize(this.Size);
+            resizeWebView();
+        }
+
+        private void Form1_Deactivate(object sender, EventArgs e)
+        {
+            showView(false);
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Right)
+            {
+                showView(true);
+            }
+        }
+
+
+        private void itemOpenInBrowser_Click(object sender, EventArgs e)
+        {
+            openBrowser();
+        }
+
+        private void itemAlignViewTopLeft_Click(object sender, EventArgs e)
+        {
+            setViewPosition("topLeft");
+        }
+
+        private void itemAlignViewTopRight_Click(object sender, EventArgs e)
+        {
+            setViewPosition("topRight");
+        }
+
+        private void itemAlignViewBottomLeft_Click(object sender, EventArgs e)
+        {
+            setViewPosition("bottomLeft");
+        }
+
+        private void itemAlignViewBottomRight_Click(object sender, EventArgs e)
+        {
+            setViewPosition("bottomRight");
+        }
+
+        private void itemSetStartURL_Click(object sender, EventArgs e)
+        {
+            string startURL = Interaction.InputBox("Enter Start URL:", "Set Start URL", currentStartURL).Trim();
+
+            if (Uri.IsWellFormedUriString(startURL, UriKind.Absolute) && startURL != "")
+            {
+                setStartURL(startURL);
+            }
+            else if (startURL == "")
+            {
+                return;
+            }
+            else
+            {
+                MessageBox.Show("Invalid URL Entered!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                itemSetStartURL.PerformClick();
+            }
+        }
+
+        private void itemStayOnTop_Click(object sender, EventArgs e)
+        {
+            setStayOnTop(!currentStayOnTop);
+        }
+
+        private void itemRestartApplication_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void itemResetApplication_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Do you really want to delete all saved settings?", "Reset Application", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                ResetSettings();
+        }
+
+        private void itemSaveCurrentSettings_Click(object sender, EventArgs e)
+        {
+            SaveCurrentSettings();
+            MessageBox.Show("Saved current settings successfully.", "Settings Saved", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void itemQuit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void resizeWebView()
+        {
+            mainWebView.Size = new Size(this.Width + 1, this.Height);
+        }
+
+
+        private void setViewPosition(string position)
+        {
+            switch (position)
+            {
+                case "topLeft":
+                    this.Location = new Point(0, 0);
+                    currentViewPosition = position;
+                    break;
+
+                case "topRight":
+                    this.Location = new Point(Screen.FromHandle(this.Handle).WorkingArea.Width - this.Width, 0);
+                    currentViewPosition = position;
+                    break;
+
+                case "bottomLeft":
+                    this.Location = new Point(0, Screen.FromHandle(this.Handle).WorkingArea.Height - this.Height);
+                    currentViewPosition = position;
+                    break;
+
+                case "bottomRight":
+                    this.Location = new Point(Screen.FromHandle(this.Handle).WorkingArea.Width - this.Width, Screen.FromHandle(this.Handle).WorkingArea.Height - this.Height);
+                    currentViewPosition = position;
+                    break;
+
+                default:
+                    setViewPosition("bottomRight");
+                    break;
+            }
+
+            RenderViewItemsChanges();
+        }
+
+        private void setStayOnTop(Boolean set)
+        {
+            this.TopMost = set;
+            currentStayOnTop = set;
+
+            RenderViewItemsChanges();
+        }
+
+        private void setStartURL(string url)
+        {
+            currentStartURL = url;
+            mainWebView.Source = new Uri(url);
+        }
+
+        private void setViewSize(Size size)
+        {
+            currentViewSize = size;
+            this.Size = size;
+        }
+
+        private void showView(Boolean set)
+        {
+            try
+            {
+                this.Visible = set;
+
+                if (set == true)
+                {
+                    this.Opacity = 100;
+                    this.Focus();
+                    this.Activate();
+                }
+                else
+                {
+                    this.Opacity = 0;
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+        }
+
+        private void RenderViewItemsChanges()
+        {
+            itemAlignViewTopLeft.Text = "Align View Top Left";
+            itemAlignViewTopRight.Text = "Align View Top Right";
+            itemAlignViewBottomLeft.Text = "Align View Bottom Left";
+            itemAlignViewBottomRight.Text = "Align View Bottom Right";
+
+            if (currentViewPosition == "topLeft")
+                itemAlignViewTopLeft.Text = "✓ Align View Top Left";
+
+            if (currentViewPosition == "topRight")
+                itemAlignViewTopRight.Text = "✓ Align View Top Right";
+
+            if (currentViewPosition == "bottomLeft")
+                itemAlignViewBottomLeft.Text = "✓ Align View Bottom Left";
+
+            if (currentViewPosition == "bottomRight")
+                itemAlignViewBottomRight.Text = "✓ Align View Bottom Right";
+
+            itemStayOnTop.Text = "Stay on Top";
+
+            if (currentStayOnTop == true)
+                itemStayOnTop.Text = "✓ Stay on Top";
+
+            showView(true);
+        }
+
+        private void openBrowser()
+        {
+            Process browser = new Process();
+            browser.StartInfo.UseShellExecute = true;
+            browser.StartInfo.FileName = mainWebView.Source.ToString();
+            browser.Start();
+        }
+
+        private void SaveCurrentSettings()
+        {
+            Settings.Default.savedStartURL = currentStartURL;
+            Settings.Default.savedViewPosition = currentViewPosition;
+            Settings.Default.savedViewSize = currentViewSize;
+            Settings.Default.savedStayOnTop = currentStayOnTop;
+
+            Properties.Settings.Default.Save();
+        }
+
+        private void LoadCurrentSettings()
+        {
+            if (Settings.Default.savedStartURL != "")
+            {
+                setStartURL(Settings.Default.savedStartURL);
+            }
+
+            if (Settings.Default.savedViewSize != new Size(0, 0))
+            {
+                setViewSize(Settings.Default.savedViewSize);
+            }
+
+            if (Settings.Default.savedViewPosition != "")
+            {
+                setViewPosition(Settings.Default.savedViewPosition);
+            }
+
+            setStayOnTop(Settings.Default.savedStayOnTop);
+
+            resizeWebView();
+        }
+
+        private void ResetSettings()
+        {
+            Settings.Default.Reset();
+            MessageBox.Show("Application has been reset.", "Application Reset", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            Application.Restart();
+        }
+    }
+}
